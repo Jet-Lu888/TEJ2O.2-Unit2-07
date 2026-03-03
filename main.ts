1f9c44b90083db33dbfaabffd2d4e1a69e15880c
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-03-03 13:14:31 -0500

    Created program

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Mar 2026
+ * This program is cookie clicker.
 */
 
-basic.showString('Hello, World!')
+let cookies = 0
+
+// adds cookies on A pressed
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    cookies = (cookies +1)
+    basic.showNumber(cookies)
+})
+
+// resets cookies on B pressed
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    cookies = 0
+    basic.showNumber(cookies)
+})
+
+    
+
+
+
